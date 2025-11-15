8d8545f159f7b7a86c296f5003ba38a37ad9cb93
Meet Patel <meet098214@gmail.com>
2025-11-15 14:36:17 +0530

    Complete Project

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://feedback-system-wheat-psi.vercel.app"   // YOUR FRONTEND URL
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
